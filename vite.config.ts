088fb32b74b9f22178e163a7db17ce75cc6eed8a
Heesnag Whang <hwhang0917@gmail.com>
2025-02-12 23:48:22 +0900

    💚 Fix ci error

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "/",
+  base: "/recurvie-maze/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
