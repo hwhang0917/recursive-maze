c359dd1fef06c01ce66a4d155c5d2de4b87e67b7
Heesnag Whang <hwhang0917@gmail.com>
2025-02-12 23:51:15 +0900

    💚 Fix ci build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "/recurvie-maze/",
+  base: "https://hwhang0917.github.io/recursive-maze/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
